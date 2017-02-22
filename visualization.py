c27e5efad84d66fac959e3f7e69688a904ec3140
CeliaYZhang <czhang2@caltech.edu>
2017-02-22 02:36:23 -0800

    state parts of speech

--- visualization.py
+++ visualization.py
@@ -1,5 +1,9 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import networkx as nx
+from preprocessing import read_word_list
 
+## Top ten words of each state. 
 def topWords(O, words, n=10):
     N = len(O)
     words = np.asarray(words)
@@ -10,4 +14,46 @@ def topWords(O, words, n=10):
         topWords[state] = words[topInd]
     return np.asarray(topWords)
 
-def 
+## Probability of words of each part of speech in each state. Returns array of 
+## dictionaries. Outer array index refers to state number. Dictionary has parts
+## of speech as keys and sums of probabilities as values. 
+def statePartsOfSpeech(O, PoS):
+    N = len(O) 
+    allProbs = [{} for i in range(N)]
+    for state in range(N):
+        probs = allProbs[state]
+        for word in range(len(O[state])):
+            pos = PoS[word]
+            if pos in probs:
+                probs[pos] += O[state][word]
+            else:
+                probs[pos] = O[state][word]
+    return allProbs
+
+#Ot = [[.7, .4, .5], [.3, .6, .8]]
+#wordst = ['a', 'b', 'c']
+#PoSt = ['NN', 'NN', 'V']
+#print statePartsOfSpeech(Ot, PoSt)
+
+"""
+## Draw Markov diagram. 
+def graphHMM(A):
+    n_states = len(A) 
+    
+    # Construct graph 
+    G = nx.DiGraph()
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos,
+                           nodelist = range(n_states),
+                           node_color = 'b',
+                           node_size = 2000,
+                           alpha = 0.5)
+    for i in range(n_states):
+        for j in range(n_states):
+            G.add_edge(i, j, weight = A[i][j])
+    nx.draw(G)
+    plt.show()
+
+At = [[.7, .4, .5], [.3, .6, .8], [.5, .6, .7]]
+graphHMM(At)
+"""
