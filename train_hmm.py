64e4c2b1101de0ffc5ce86a2092a620075bd6277
Robert Eng <robert.km.eng@gmail.com>
2017-02-23 00:59:14 -0800

    wooops, csv to json.

--- train_hmm.py
+++ train_hmm.py
@@ -66,6 +66,6 @@ def main():
     X = read_data('data/spenspear/reverse_num_tokenized.json')
     
     # Change the number of states and model name here before your run!!!!!!!
-    train(X, 10, 'spenspear_10_states')
+    train(X, 12, 'spenspear_12_states')
 
 main()
