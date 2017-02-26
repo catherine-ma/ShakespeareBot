61943be7dde2bbd4f899d5c154fe8a676eb6d2e3
Catherine <catherinema828@gmail.com>
2017-02-26 13:53:23 -0800

    finished lstm

--- deeplearning.py
+++ deeplearning.py
@@ -5,63 +5,82 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
-import sys
 
+import nltk
 import preprocessing as pp
 
-spear = 'data/shakespeare.txt'
-spens = 'data/spenser.txt'
-tok_words = 'data/spenspear/tokenized_words.json'
+toke_lines = 'data/spenspear/tokenized_words.json'
+ham = 'hamlet.txt'
 
-def process_text_spear(file_name):
+def process_text_ham(file_name):
     with open(file_name) as data_file:
         lines = data_file.readlines()
 
-    poem_lines = []
+    play_lines = []
 
     for i in range(len(lines)):
-        if lines[i][0:3] != '   ' and lines[i] != '\n':
+        if '=' not in lines[i] and 'Scene' not in lines[i] \
+        and 'ACT' not in lines[i] and lines[i] != '\r\n':
             lines[i] = lines[i].lower()
-            poem_lines.append(lines[i].strip())
+            lines[i] = lines[i].strip()
 
-    return '\n'.join(poem_lines)
+            play_lines.append(lines[i])
 
-def process_text_spens(file_name):
-    with open(file_name) as data_file:
-        lines = data_file.readlines()
+    return play_lines
+
+def tokenize_ham(lines):
+    toke_lines = [nltk.word_tokenize(l) for l in lines]
     
-    poem_lines = []
+    new_lines = []
+    count = 0
 
-    for i in range(len(lines)):
-        if len(lines[i]) >= 10:
-            lines[i] = lines[i].lower()
-            poem_lines.append(lines[i].strip())
+    for line in toke_lines:
+        new_line = []
 
-    return '\n'.join(poem_lines)
+        for word in line:
+            if word.isalpha:
+                new_line.append(word)
 
-def process_words(file_name):
-    lines = pp.read_data(file_name)
-    max_len = -1
+        new_lines.append(new_line)
+
+    toke_lines = new_lines
+
+    return toke_lines
+
+def elim_punct(lines):
+    no_punct_lines = []
+
+    for line in lines:
+        no_punct_line = []
+
+        for word in line:
+            if word.isalpha():
+                no_punct_line.append(word)
+
+        no_punct_lines.append(no_punct_line)
+
+    return no_punct_lines
+
+def lines_to_words(lines):
     words = []
 
     for line in lines:
         for word in line:
-            max_len = max(max_len, len(word))
             words.append(word)
-
-    # for i in range(len(words)):
-    #     words[i] = words[i] + (max_len - len(words[i])) * ' '
 
     return words
 
-    return X, y
-
 if __name__ == '__main__':
-    # spear_text = process_text_spear(spear)
-    # spens_text = process_text_spens(spens)
-    # text = spear_text + spens_text
-    text = process_words(tok_words)
+    # Gets list of words from Hamlet and sonnets
+    play_lines = process_text_ham(ham)
+    toke_play_lines = tokenize_ham(play_lines)
+    no_punct_play_lines = elim_punct(toke_play_lines)
+    sonnet_lines = pp.read_data(toke_lines)
+    no_punct_sonnet_lines = elim_punct(sonnet_lines)
+    lines = no_punct_play_lines + no_punct_sonnet_lines
+    text = lines_to_words(lines)
 
+    # Prepare data for model
     words = sorted(list(set(text)))
     word_to_int = dict((c, i) for i, c in enumerate(words))
     int_to_word = dict((i, c) for i, c in enumerate(words))
@@ -88,34 +107,62 @@ if __name__ == '__main__':
 
     model = Sequential()
     model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
-    # model.add(Dropout(0.2))
     model.add(Dense(y.shape[1], activation='softmax'))
-    filename = 'weights-improvement-493-0.1677.hdf5'
+    filename = 'weights/weights-improvement-249-1.1625.hdf5'
     model.load_weights(filename)
     model.compile(loss='categorical_crossentropy', optimizer='adam')
                              
-    # filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+    # filepath='weights/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5'
     # checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
     # callbacks_list = [checkpoint]
 
-    # model.fit(X, y, nb_epoch=500, batch_size=64, callbacks=callbacks_list)
+    # model.fit(X, y, nb_epoch=250, batch_size=64, callbacks=callbacks_list)
 
     start = np.random.randint(0, len(dataX)-1)
     pattern = dataX[start]
-    print "\"", ' '.join([int_to_word[value] for value in pattern]), "\""
-    # generate characters
-    res = []
-    for i in range(140):
+    print 'Seed Pattern: ' + ' '.join([int_to_word[value] for value in pattern])
+    sonnet_words = []
+    for i in range(112):
         x = np.reshape(pattern, (1, len(pattern), 1))
         x = x / float(n_vocab)
         prediction = model.predict(x, verbose=0)
         index = np.argmax(prediction)
         result = int_to_word[index]
         seq_in = [int_to_word[value] for value in pattern]
-        res.append(result)
+        sonnet_words.append(result)
         pattern.append(index)
         pattern = pattern[1:len(pattern)]
-    print "\nDone."
 
-    ' '.join(res)
-    print res
+    sonnet = []
+    line = []
+
+    for i in range(112):
+        if (i + 1) % 8 == 0:
+            sonnet.append(line)
+            line = []
+        else:
+            line.append(sonnet_words[i])
+
+    for i in range(14):
+        new_line = ' '.join(sonnet[i])
+        new_line = new_line.capitalize()
+        sonnet[i] = new_line
+        
+    print '\n'.join(sonnet)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
